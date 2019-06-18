379c8d76eb8d35edd2536ac81bfb68a9f05591b9
CHERUPALLY <karuna.cherupally@capgemini.com>
2019-06-18 15:02:03 +0530

    19-07-2019.

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
-var config = require('./config');
-// const {Datastore} = require('@google-cloud/datastore');
-
-// // Creates a client
-// const datastore = new Datastore({
-//     projectId: config.projectId,
-//     keyFilename: config.keyFilename
-// });
-
-
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-    // var query = datastore.createQuery(['customer'])
-    // datastore.runQuery(query,(err,customers)=>{
-    //     if(err){
-    //         console.log(err)
-    //         res.send("No customers!!")
-    //     }  
-    //     res.send(customers);      
-    // });
     res.send('HEELLOOO');
 });
 
